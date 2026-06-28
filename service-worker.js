34f5535e53f063aa96242e29e70ece137f334f44
trefilini-gif <trefilini@gmail.com>
2026-06-28 14:31:07 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,25 @@
-const CACHE_NAME = 'workout-timer-v1';
-const FILES = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'workout-card-timer-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key === CACHE_NAME ? null : caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
